47a69f0d617722494228f39badb48c3b47ab1998
Ivan Arredondo <68788617+ivan-arredondom@users.noreply.github.com>
2026-05-03 20:36:47 -0700

    part 2

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,7 +3,7 @@ export const CONFIG = {
   // Site Settings
   // ---------------------------------------------------------------------------
   site: {
-    url: "https://alexmercer.dev",
+    url: "https://ivan-arredondom.github.io",
     locale: "en_US",
     twitterHandle: "@alexmercer_dev",
   },
